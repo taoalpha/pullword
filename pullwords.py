a5ea52611e797a3a4e008a8300f3b4eb5f25e99e
zzgary <zzgary92@gmail.com>
2013-05-25 13:52:54 +0800

    update & Refactor
    filter all word and get rid of some similar words.eg: "word" "words"---keep the "word" and remove "words"

--- pullwords.py
+++ pullwords.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import sys
 import codecs
 import re
-import collections
+# import collections
 #import pyPdf
 
 
@@ -9,16 +12,40 @@ def coded(filename):
     sentence = []
     try:
         f = codecs.open(filename, "r", encoding="utf-16")
-        for line in f:
-            sentence.append(line)
+        strs = f.read()
+        sentence = re.findall("\w+", str.lower(strs))
+        # for line in f:
+        #     sentence.append(line)
     except UnicodeError:
         f = open(filename, "r")
-        for line in f:
-            sentence.append(line)
+        strs = f.read()
+        sentence = re.findall("\w+", str.lower(strs))
+        # for line in f:
+        #     sentence.append(line)
     #for line in codecs.open(filename, "r", encoding="utf-16").readlines():
     #    if line != "\r\n" and line != "111\r\n":
     #        sentence.append(line)
     return sentence
+
+
+def removeNonAscii(s):
+    return ("".join(filter(lambda x: ord(x) < 128 and ord(x) > 64, s)))
+
+
+def compared(word_l):
+    # word_l = []
+    # for word, count in words:
+    #     word_l.append(word)
+    try:
+        for i in range(len(word_l)):
+            if len(word_l[i]) > 2:
+                wordf = word_l[i]
+                for word in word_l[i + 1:i + 10]:
+                    if word.startswith(wordf) and (len(word) - len(wordf)) < 4:
+                        word_l.remove(word)
+    except:
+        pass
+    return word_l
 
 
 def init(filename, alpha):
@@ -26,29 +53,30 @@ def init(filename, alpha):
     sent1 = []
     word0 = []
     word_a = []
+    # for li in sentence[:]:
+    #     if not(li.startswith("00")):
+    #         sent1.append(li)
     for li in sentence[:]:
-        if not(li.startswith("00")):
-            sent1.append(li)
-    for li in sent1[:]:
-        li = re.sub('[,.!?":\[\];()]', " ", li)
-        for wds in li.split():
+        li = re.sub('[-,,.!?":\[\]\';()~]', " ", li)
+        wds = removeNonAscii(li)
+        if len(wds) > 3 and not wds.startswith("aa"):
             word0.append(wds)
     for words in word0[:]:
-        if words.startswith(alpha) or words.startswith(alpha.upper()):
-            words = words.lower()
+        if words.startswith(alpha) or words.startswith(alpha.lower()):
             word_a.append(words)
-    word_c = collections.Counter(word_a)
-    word_a = word_c.most_common(1000000000)
+    # word_c = collections.Counter(word_a)
+    # word_a = word_c.most_common(1000000000)
+    word_a = list(set(word_a))
     word_a.sort()
+    word_a = compared(word_a)
+    # for i in range(len(word_a)):
+    #     word_a = compared(word_a)
     print "The words starting with %r in %r list below:" % (alpha, filename)
-    for word, count in word_a:
-        print "{0:<20s}: {1:>8d}".format(word, count)
-    #print "\n".join('%s %s' % (x, y) for x in word_c.keys() for y in word_c.values())
-    #word_a = list(set(word_a))
-    #print "\n".join(word_c)
+    for word in word_a:
+        print "{0:<20s}: {1:>8d}".format(word, sentence.count(word))
 
 
-#def fftype(ffname):
+# def fftype(ffname):
 
 
 if __name__ == '__main__':
@@ -56,5 +84,6 @@ if __name__ == '__main__':
     if len(sys.argv) == 3:
         init(sys.argv[1], sys.argv[2])
     if len(sys.argv) == 2:
-        alpha = raw_input(">>Please input the character you want to pull the words start with:")
+        alpha = raw_input(">>Please input the character \
+            you want to pull the words start with:")
         init(sys.argv[1], alpha)
